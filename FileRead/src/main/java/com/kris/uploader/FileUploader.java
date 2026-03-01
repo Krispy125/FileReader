cfd88ff6daddf65aeb69278206fb7b44c71ea6cb
Krispy125 <kburns19@rockets.utoledo.edu>
2026-03-01 13:30:22 -0500

    first commit

---
+++ FileRead/src/main/java/com/kris/uploader/FileUploader.java
@@ -0,0 +1,11 @@
+package com.kris.uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploader {
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploader.class, args);
+	}
+}
